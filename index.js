188e18bb6db780546e1bcdb3a23f8bdf730a0ca5
wolyshaw <shaw@xwlong.com>
2016-10-01 10:56:14 +0800

    change dirname

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 import {Router, Route, IndexRoute, browserHistory} from 'react-router'
-import home from './src/home/home'
-import page from './src/page/page'
-import post from './src/post/post'
+import home from './src/home/'
+import page from './src/page/'
+import events from './src/events/'
+import post from './src/post/'
 import headerNav from './src/common/siteHeader/headerNav'
+
+console.log(events)
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={headerNav}>
       <IndexRoute component={home}/>
-      <Route path="/page" component={page}/>
+      <Route path="/events/:id" component={events}/>
+      <Route path="/page/:id" component={page}/>
       <Route path="/post/:id" component={post} />
     </Route>
   </Router>
---
+++ src/events/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import {Link} from 'react-router'
+import HomePostList from '../home/homePostList'
+import {objToStr, ajax} from '../util/util'
+
+export default class events{
+
+}
