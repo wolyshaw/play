528cd225a3e7987a415bd1cbb04237f49714308e
wolyshaw <shaw@xwlong.com>
2016-10-06 15:39:03 +0800

    change path

--- src/util/util.js
+++ src/util/util.js
@@ -15,7 +15,7 @@ export function showLoading(show){
   }
 }
 export function ajax(set) {
-  var url = 'http://local.abcdea.net' + set.url,
+  var url = 'http://demo.abcdea.net/api' + set.url,
     data = set.data === undefined ? null : objToStr(set.data),
     type = set.type || 'get',
     success = set.success,
