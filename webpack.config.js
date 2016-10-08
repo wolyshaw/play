f7a1d451bfb62813dd0d61d5b0fde7240bd3fc77
wolyshaw <shaw@xwlong.com>
2016-10-08 20:09:37 +0800

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
       compress: {
         warnings: false
       }
+    })，
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: '"production"'
+        }
     })
   ]
 }
