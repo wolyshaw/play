1b6a8aacc7a08a510b260c9d3d6355c88ac5b854
wolyshaw <wolyshaw@users.noreply.github.com>
2017-11-05 20:10:00 +0800

    更新npm模块

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        loader: 'babel-loader?presets[]=env&presets[]=react'
       },
       {
         test: /\.(css|less)$/,
